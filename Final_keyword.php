72676354a97a0c9ffe8988b75a17d11895a736b7
dev <105196@ondirect.in>
2026-06-15 17:04:33 +0530

    updatet final method and description

--- Final_keyword.php
+++ Final_keyword.php
@@ -1,12 +1,26 @@
 <?php
-final class honda{
+// final class honda{
 
+// }
+
+// class car extends honda{
+
+// }
+// $c= new car();
+
+class honda{
+    final function cname(){
+        echo 'honda';
+    }
 }
 
 class car extends honda{
-
+     function cname(){
+        echo 'honda city';
+    }
 }
 $c= new car();
+$c->cname();
 
 
 // "final" class means we can't inherit the class using the "final" keyword.
@@ -18,4 +32,6 @@ $c= new car();
 
 // If we try to override a final method, we get an error like this:
 // Fatal error: Cannot override final method Honda::drive() in C:\xampp\htdocs\gitdemo\php-Q-A\Final_keyword.php on line 9
+
+//only the final method can't be overridden, but normal (non-final) methods can be overridden, and both are inherited and usable in the child class as normal.
 ?>
