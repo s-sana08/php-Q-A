e002db2513ce47dc2ac89ddb95ed1248a03dd3da
dev <105196@ondirect.in>
2026-06-15 17:16:42 +0530

    add more about final keyword

--- Final_keyword.php
+++ Final_keyword.php
@@ -23,15 +23,16 @@
 // $c->cname();
 
 
-// "final" class means we can't inherit the class using the "final" keyword.
+/* "final" class means we can't inherit the class using the "final" keyword.
 
-// If we try to inherit a final class, we get an error like this:
-// Fatal error: Class Car may not inherit from final class (Honda) in C:\xampp\htdocs\gitdemo\php-Q-A\Final_keyword.php on line 9
+If we try to inherit a final class, we get an error like this:
+Fatal error: Class Car may not inherit from final class (Honda) in C:\xampp\htdocs\gitdemo\php-Q-A\Final_keyword.php on line 9
 
-// A "final" method means we can't override the method in a child class using the "final" keyword.
+A "final" method means we can't override the method in a child class using the "final" keyword.
 
-// If we try to override a final method, we get an error like this:
-// Fatal error: Cannot override final method Honda::drive() in C:\xampp\htdocs\gitdemo\php-Q-A\Final_keyword.php on line 9
+If we try to override a final method, we get an error like this:
+Fatal error: Cannot override final method Honda::drive() in C:\xampp\htdocs\gitdemo\php-Q-A\Final_keyword.php on line 9
 
-//only the final method can't be overridden, but normal (non-final) methods can be overridden, and both are inherited and usable in the child class as normal.
+only the final method can't be overridden, but normal (non-final) methods can be overridden, and both are inherited and usable in the child class as normal.
+*/
 ?>
